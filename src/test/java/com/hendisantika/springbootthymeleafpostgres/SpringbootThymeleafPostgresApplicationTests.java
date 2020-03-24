43872d0db3ee656d2bb37696dbede76c1e955f7e
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-25 06:01:34 +0700

    #1 - Initial Commit

---
+++ src/test/java/com/hendisantika/springbootthymeleafpostgres/SpringbootThymeleafPostgresApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.springbootthymeleafpostgres;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootThymeleafPostgresApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
