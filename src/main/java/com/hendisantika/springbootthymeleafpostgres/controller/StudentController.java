f629c9980e3337cc4a087f23b0cd4d301af5d070
Hendi Santika <hendisantika@yahoo.co.id>
2020-03-25 06:15:08 +0700

    #16 - Add editForm in StudentController

--- src/main/java/com/hendisantika/springbootthymeleafpostgres/controller/StudentController.java
+++ src/main/java/com/hendisantika/springbootthymeleafpostgres/controller/StudentController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 
 /**
@@ -40,4 +41,9 @@ public class StudentController {
         return "redirect:/";
     }
 
+    @GetMapping(value = "/edit/{id}")
+    public String editForm(@PathVariable Integer id, Model model) {
+        model.addAttribute("student", studentRepository.getOne(id));
+        return "formStudent";
+    }
 }
