545d3c2945bf417a93e4c4035ad92e367aa709d3
hendisantika <hendisantika@yahoo.co.id>
2024-05-26 09:09:22 +0700

    Change package & config

--- src/main/java/com/hendisantika/springbootthymeleafpostgres/entity/Student.java
+++ src/main/java/com/hendisantika/springbootthymeleafpostgres/entity/Student.java
@@ -1,16 +1,15 @@
 package com.hendisantika.springbootthymeleafpostgres.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
-import javax.persistence.Table;
-import javax.persistence.Version;
+import org.springframework.data.annotation.Version;
 
 /**
  * Created by IntelliJ IDEA.
